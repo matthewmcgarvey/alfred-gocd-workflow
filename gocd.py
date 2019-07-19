a4fa52158043576f691441d2f5a1b73fff18971a
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2019-07-19 13:04:27 -0500

    prompt for password on refresh instead of storing it

--- gocd.py
+++ gocd.py
@@ -1,6 +1,8 @@
 # encoding: utf-8
 import sys
-from workflow import Workflow3, ICON_WEB, web, ICON_WARNING, ICON_ERROR
+from workflow import Workflow3, web, ICON_WEB, ICON_WARNING, ICON_ERROR
+
+log = None
 
 def get_config(wf):
     config = wf.stored_data('config')
@@ -8,19 +10,10 @@ def get_config(wf):
         return {}
     return config
 
+
 def save_config(wf, config):
     wf.store_data('config', config)
 
-def get_password(wf):
-    try:
-        return wf.get_password("alfred-gocd-pw")
-    except:
-        wf.add_item("You must supply a password", icon=ICON_ERROR)
-        wf.send_feedback()
-        sys.exit(1)
-
-def save_password(wf, pw):
-    wf.save_password("alfred-gocd-pw", pw)
 
 def get_username(config):
     username = config.get('username')
@@ -31,6 +24,7 @@ def get_username(config):
     else:
         return username
 
+
 def get_base_url(config):
     base_url = config.get('base_url')
     if base_url is None:
@@ -40,23 +34,31 @@ def get_base_url(config):
     else:
         return base_url
 
-def load_pipelines(wf, config):
-    stored_pipelines = wf.stored_data("pipelines") # ["spring-extensions", "skybridge"]
-    if stored_pipelines is not None and len(stored_pipelines):
-        return stored_pipelines
-    base_url = get_base_url(config)
-    url = "%s/go/api/config/pipeline_groups" % base_url
-    username = get_username(config)
-    password = get_password(wf)
 
-    r = web.get(url, auth=(username, password))
-    r.raise_for_status()
-    data = r.json()
-    pipelines = [pipeline['name']
-                    for group in data
-                    for pipeline in group['pipelines']]
-    wf.store_data('pipelines', pipelines)
-    return pipelines
+def load_pipelines(wf, config, password=None, refresh=False):
+    if not refresh:
+        stored_pipelines = wf.stored_data("pipelines")
+        if stored_pipelines is not None and len(stored_pipelines):
+            log.debug("Found cached pipelines")
+            return stored_pipelines
+        log.debug("Did not find cached pipelines, proceeding to load")
+    try:
+        base_url = get_base_url(config)
+        url = "%s/go/api/config/pipeline_groups" % base_url
+        username = get_username(config)
+
+        r = web.get(url, auth=(username, password))
+        r.raise_for_status()
+        data = r.json()
+        pipelines = [pipeline['name']
+                        for group in data
+                        for pipeline in group['pipelines']]
+        wf.store_data('pipelines', pipelines)
+        return pipelines
+    except Exception as e:
+        log.exception(e)
+        return False
+
 
 def add_item(wf, pipeline, base_url):
     pipeline_url = "%s/go/tab/pipeline/history/%s" % (base_url, pipeline)
@@ -66,9 +68,11 @@ def add_item(wf, pipeline, base_url):
                 uid=pipeline_url,
                 valid=True)
 
+
 def add_items(wf, pipelines, base_url):
     for pipeline in pipelines:
         add_item(wf, pipeline, base_url)
+
 
 def main(wf):
     args = wf.args
@@ -86,18 +90,16 @@ def main(wf):
         config['username'] = username
         save_config(wf, config)
         return
-    elif keyword_arg == '--auth-pw':
-        pw = " ".join(args[1:])
-        save_password(wf, pw)
-        return
     elif keyword_arg == '--baseurl':
         base_url = args[1]
         config['base_url'] = base_url
         save_config(wf, config)
         return
     elif keyword_arg == "--refresh":
-        wf.clear_data(lambda f: "pipelines" in f)
-        load_pipelines(wf, config)
+        pw = " ".join(args[1:])
+        result = load_pipelines(wf, config, pw, refresh=True)
+        sys.stdout.write("success" if result else "failure")
+        sys.stdout.flush()
         return
     else:
         query = keyword_arg
@@ -110,7 +112,7 @@ def main(wf):
         wf.send_feedback()
 
 
-
 if __name__ == u"__main__":
     wf = Workflow3(libraries=['./lib'])
+    log = wf.logger
     sys.exit(wf.run(main))
